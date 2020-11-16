14ca60282c2391f6449ab1ae4f359205a0f3cdcb
Nadeem012 <55021916+ndm81@users.noreply.github.com>
2020-11-16 22:22:26 +0530

    Add files via upload

---
+++ UrlConnection.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.net.*;
+public class UrlConnection{
+	public static void main(String[] args) {
+		try{
+			URL url=new URL("http://www.smartcoder.com/java-code");
+			URLConnection urlcon=url.openConnection();
+			InputStream stream=urlcon.getInputStream();
+			int i;
+			while((i=stream.read())!=-1){
+				System.out.println((char)i);
+			}
+		}catch(Exception e){System.out.println(e);}
+	}
+}
